e74e7e3974e65ee158cbca32e0897b5a586771de
ZhaobinMo <thumozb@gmail.com>
2019-02-03 14:50:14 -0500

    sm12

--- task2/task21.py
+++ task2/task21.py
@@ -5,3 +5,7 @@ Created on Sat Feb  2 21:20:39 2019
 @author: Zhaobin
 """
 
+import pandas as pd
+
+df_population = pd.read_excel("Annual Estimats of Resident Population 2010-2018.xlsx")
+df_puerto = pd.read_excel("Puerto Rico Commonwealth Population Totals and Components of Change 2010-2018.csv")
